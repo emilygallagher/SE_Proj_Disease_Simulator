088f37fc235ca4bafeb80e6aaff940a9f57da30e
THOMAS KOHUT <thomaskohut@mail.adelphi.edu>
2020-12-04 21:12:52 -0500

    Checks & Validations
    Added Checks & Validations to UI Entries

--- com/sim/UITest.java
+++ com/sim/UITest.java
@@ -40,6 +40,10 @@ import javafx.stage.FileChooser.ExtensionFilter;
  */
 public class UITest extends Application
 {
+    private String el = "", el2 = "";
+    private boolean isValid, isValid2;
+    private final int MAXPOP = 2500;
+
     public static void main(String[] args)
     {
         try
@@ -56,8 +60,9 @@ public class UITest extends Application
         }
     }
 
-    public void start(Stage mainStage)
+    public void start(Stage mainStage) throws Exception
     {
+        //Sets up application
         mainStage.setTitle("Disease Simulator");
 
         VBox root2 = new VBox();
@@ -71,36 +76,33 @@ public class UITest extends Application
         root.setTop(bar);
         root.setCenter(root2);
 
-        // custom code below --------------------------------------------
         GridPane designArea = new GridPane();
         designArea.setHgap(5);
         designArea.setVgap(9);
         designArea.setPadding( new Insets(8) );
 
-        // ------------------Population Information-----------------------------
-        HBox row1 = new HBox();
         // ------------------Days section-------------------------------------------
-        //TextField DayInt  = new TextField();DayInt.setMaxWidth(80);
         CheckBox CheckDay = new CheckBox("Simulate days?");
 
         // -------------------Button--------------------------------------
         Button startButton = new Button("Start");
-        // --------------------------------------------------------------
 
         // region where the sim will go. it is white to display the locale
         int canvasWidth = 800;
         int canvasHeight = 800;
         Canvas canvas = new Canvas(canvasWidth, canvasHeight);
         GraphicsContext context = canvas.getGraphicsContext2D();
-        context.setFill( Color.WHITE );
+        context.setFill(Color.WHITE);
         context.fillRect(0,0, canvasWidth,canvasHeight);
+        
+        //Inserts UI into screen
         HBox row2 = new HBox();// adds in the names for HBoxes
         row2.getChildren().addAll(designArea, canvas);
         root2.getChildren().addAll(row2);
 
         //Label array
         Label[] labs = new Label[5];
-        String[] labStr = {"Diseases:","Preventative Measure:","Total Population","Infected Population","Days to Simulate"};
+        String[] labStr = {"Disease:","Preventative Measure:","Total Population","Infected Population","Days to Simulate"};
 
         //TextField array
         TextField[] tf = new TextField[3];
@@ -131,13 +133,13 @@ public class UITest extends Application
                                 }
                             }
                         }
-                });
+                    });
             }
         }
 
         //Adds options to combo boxes
-        cb[0].getItems().addAll("Choose one", "Covid-19", "Disease 2", "Disease 3");
-        cb[1].getItems().addAll("Choose one", "Masks", "Distance", "Masks and Distance");
+        cb[0].getItems().addAll("Choose one", "COVID-19", "Disease 2", "Disease 3");
+        cb[1].getItems().addAll("Choose one", "None", "Masks", "Distance", "Masks and Distance");
 
         //Sets Default value
 
@@ -152,25 +154,29 @@ public class UITest extends Application
         // event and listener to activate on changes.
 
         // most generic functional interface: no inputs, no output, contains method run()
-        Runnable updateFunction =
-            () ->
-            {
-                //to obtain text box data it is a simple .getText function.
-                if(cb[0].getValue()=="")//"" is the name in the dropdown box also they will be if statements.
-                {}
-                if ( CheckDay.isSelected() )//checking if a box was checked if it is it will run this code
-                {   }
-            };
+        // Runnable updateFunction =
+            // () ->
+            // {
+                // //to obtain text box data it is a simple .getText function.
+                // if(cb[0].getValue()=="")//"" is the name in the dropdown box also they will be if statements.
+                // {
 
-        EventHandler<ActionEvent> renderHandler = (ActionEvent event) ->{
-                updateFunction.run();
-            };
+                // }
+                // if ( CheckDay.isSelected() )//checking if a box was checked if it is it will run this code
+                // {
 
-        ChangeListener<Object> renderListener =
-            (ObservableValue<? extends Object> ov, Object oldValue, Object newValue) ->
-            {
-                updateFunction.run();
-            };
+                // }
+            // };
+
+        // EventHandler<ActionEvent> renderHandler = (ActionEvent event) ->{
+                // updateFunction.run();
+            // };
+
+        // ChangeListener<Object> renderListener =
+            // (ObservableValue<? extends Object> ov, Object oldValue, Object newValue) ->
+            // {
+                // updateFunction.run();
+            // };
 
         // This is the menu bar section that is at the top
         Menu aboutMenu = new Menu("Options");
@@ -228,12 +234,164 @@ public class UITest extends Application
                 System.exit(0);// this exits the program
             }); 
 
+        //Variables used to start
+        String[] dData = new String[2];
+        int[] iData = new int[3];
+        Alert ea = new Alert( AlertType.ERROR );
+        ea.setTitle("Error!");
+        Stage as = (Stage)ea.getDialogPane().getScene().getWindow();
+
         //start button functionality
         startButton.setOnAction((ActionEvent event) ->
             {
+                if(!TFValidationCheck(labs, tf, cb, ea)) { //|| !CBTValidationCheck(labs, cb, ea)) {
+                    ea.showAndWait();
+                } else {
+                    //Handles Textfields and ComboBoxes 
+                    for(int x = 0; x < iData.length; x++) {
+                        if(x <= 1) { //Combo Boxes
 
-            
+                        }
+                        iData[x] = Integer.parseInt(tf[x].getText()); //Text Fields
+                    }
+                }
+
             });
         mainStage.show();
     }
+
+    /**Checks each user input is valid. 
+     * True: All inputs are valid
+     * False: At least one input is invalid. Adds invalid entry to string to be used in Alert Box.
+     */
+    private boolean TFValidationCheck(Label[] labs, TextField[] tf, ComboBox<String>[] cbt, Alert ea) {
+        el = "";
+        isValid = true;
+
+        //Checks if each entry is invalid
+        for (int x = 0; x < tf.length; x++) {
+            //Handles ComboBoxes
+            if(x==0) {
+                //Checks if an option was chosen in each Combo Box
+                if(cbt[x].getValue()=="Choose one")
+                {
+                    el = el + "Please select a " + labs[x].getText() + ".\n";
+                    isValid = false;
+
+                    if(cbt[x+1].getValue()=="Choose one") {
+                        el = el + "Please select a " + labs[x+1].getText() + ".\n";
+                    }
+                }
+            }
+
+            //Checks for empty TextFields or negative numbers
+            if(tf[x].getText().isEmpty() || Integer.parseInt(tf[x].getText()) < 0) {
+                el = el + labs[x+2].getText() + " is invalid. Please enter a valid number.\n";
+
+                isValid = false;
+            } 
+
+            //Checks if Infected Population exceeds Total Population
+            if(x==1 && !tf[x].getText().isEmpty() && 
+            Integer.parseInt(tf[1].getText()) > Integer.parseInt(tf[0].getText())) {
+                el = el + "Infected Population cannot exceed Total Population.\n";
+                isValid = false;
+            }
+
+            //Checks if any value exceeds max
+            if(x <= 1 && !tf[x].getText().isEmpty() && 
+            Integer.parseInt(tf[x].getText()) > MAXPOP) {
+                el = el + labs[x+2].getText() + 
+                " exceeds the maximum limit. Please lower your entry by at least " 
+                + (Integer.parseInt(tf[x].getText()) - MAXPOP) + ".\n";
+                isValid = false;
+            }
+
+            //Displays error message if anything is caught
+            if(x == tf.length-1 && !isValid) {
+                ea.setContentText(el);
+                return isValid;
+            }
+        }
+
+        return isValid;
+    }
 }
+
+//scrap
+
+//ComboBox Validation Method
+
+// /**Checks each selection in ComboBox is valid. 
+// * True: All selections are valid
+// * False: At least one selection
+// */
+// private boolean CBTValidationCheck(Label[] labs, ComboBox<String>[] cbt, Alert ea) {
+// el2 = "";
+// isValid2 = true;
+
+// for (int x = 0; x < cbt.length; x++) {
+// //"" is the name in the dropdown box also they will be if statements.
+// if(cbt[x].getValue()=="Choose one")
+// {
+// el2 = el2 + "Please select a " + labs[x].getText() + ".\n";
+// isValid2 = false;
+// }
+
+// //Displays error message if anything is caught
+// if(x == cbt.length-1 && !isValid2) {
+// el = el + "\n" + el2;
+// ea.setContentText(el);
+// return isValid2;
+// }
+// }
+// return isValid2;
+// }
+
+//Refined version of below
+// if(TFValidationCheck(labs, tf, ea)) {
+// for (int x = 0; x < iData.length; x++) {
+// iData[x] = Integer.parseInt(tf[x].getText());
+// }
+// } else { 
+// if(CBTValidationCheck(labs, cb, ea)) {
+// for(int x = 0; x < cb.length; x++) {
+// }
+// } else {
+// ea.showAndWait();
+// }}
+
+//Base validation
+// el = "";
+// isValid = true;
+// try{
+// //Handles Textfields
+// for (int x = 0; x < iData.length; x++) {
+// //Checks if each entry is invalid (exceeds max, 
+// //if infected pop > total pop, etc). Throws error if true
+// if(tf[x].getText().isEmpty() || Integer.parseInt(tf[x].getText()) < 0) {
+// el = el + "\nInvalid entry at " + labs[x+2].getText() + 
+// ". Please enter a valid number.";
+
+// isValid = false;
+// } //else { if(x==1 && iDataInts[0] > Integer.parseInt(tf[x].getText())) {
+// else {
+
+// iData[x] = tf[x].getText();
+
+// iDataInts[x] = Integer.parseInt(iData[x]);
+
+// System.out.println(iDataInts[x]);
+// }
+
+// if(x == iData.length-1 && !isValid) {
+// ea.setContentText(el);
+// ea.showAndWait();
+// }
+// } 
+// }
+// catch(Exception e) {
+// ea.setContentText("Enter Finalized Credits");
+
+// Stage alertStage = (Stage)ea.getDialogPane().getScene().getWindow();
+// ea.showAndWait();
